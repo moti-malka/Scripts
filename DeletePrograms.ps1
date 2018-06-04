7808e1f7110f6b8d3a436056e518ba2adddb508d
Moti Malka <Moti@u-btech.com>
2018-06-04 11:55:21 +0300

    ps1 deleteprograms

---
+++ DeletePrograms.ps1
@@ -0,0 +1,11 @@
+﻿Param(
+  [Parameter(Mandatory=$True,Position=1)]
+  [string]$ProgramName
+)
+
+
+$app = Get-WmiObject -Class Win32_Product | Where-Object { 
+    $_.Name -match $ProgramName 
+}
+
+$app.Uninstall()

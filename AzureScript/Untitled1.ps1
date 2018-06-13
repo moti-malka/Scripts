6627d4a7078b78b0d4548cea0fcf6c46b1b7962e
Moti Malka <Moti@u-btech.com>
2018-06-13 14:48:32 +0300

    Signed-off-by: Moti Malka <Moti@u-btech.com>

--- AzureScript/Untitled1.ps1
+++
@@ -1,4 +0,0 @@
-﻿$date = '25/12/2012 09:25'
-$date = $date -as [datetime]
-
-$date
---
+++ AzureScript/VmHandler.ps1
@@ -0,0 +1,6 @@
+﻿#-- this script handeler vm on azure --#
+
+#Login to Azure
+Login-AzureRmAccount
+
+Stop-AzureVM -ServiceName "VmContactSync" -Name "VmContactSync"

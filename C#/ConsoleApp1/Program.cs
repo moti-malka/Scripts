9b476b1f957c1da583e92518fd5e79f7fc14c635
Moti Malka <Moti@u-btech.com>
2018-06-14 19:24:41 +0300

    auto print email 1. search in folder "DalkanimPrint" 2. search in mail "maildalkanim@sonol.co.il" 3. download file and send the file to printer

--- C#/ConsoleApp1/Program.cs
+++ C#/ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.Configuration;
 using Microsoft.Office.Interop.Outlook;
 using System.IO;
+using System.Diagnostics;
 
 namespace ConsoleApp1
 {
@@ -15,14 +16,14 @@ namespace ConsoleApp1
     {
 
         // Path where attachments will be saved
-        static string basePath = @"c:\temp\emails\";
+        static string basePath = @"c:\temp\emails";
         static int totalfilesize = 0;
 
         static void Main(string[] args)
         {
             EnumerateAccounts();
             EnumerateFoldersInDefaultStore();
-            //Console.WriteLine("Total file size:" + totalfilesize);
+           
         }
 
         static void EnumerateFoldersInDefaultStore()
@@ -40,26 +41,25 @@ namespace ConsoleApp1
             {
                 foreach (Microsoft.Office.Interop.Outlook.Folder childFolder in childFolders)
                 {
-
-                 
                     // We only want Inbox folders - ignore Contacts and others
-                    if (childFolder.FolderPath.Contains("test"))
+                    if (childFolder.FolderPath.Contains("DalkanimPrint"))
                     {
-                        // Write the folder path.
-                        Console.WriteLine(childFolder.FolderPath);
+                        
                         // Call EnumerateFolders using childFolder, to see if there are any sub-folders within this one
                         EnumerateFolders(childFolder);
                     }
                 }
             }
-            Console.WriteLine("Checking in " + folder.FolderPath);
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Checking in.... " + folder.FolderPath);
+            Console.ForegroundColor = ConsoleColor.White;
             IterateMessages(folder);
         }
 
         static void IterateMessages(Microsoft.Office.Interop.Outlook.Folder folder)
         {
             // attachment extensions to save
-            string[] extensionsArray = { ".txt", ".csv"};
+            string[] extensionsArray = {".docx", "xlsx", ".pdf", ".doc"};
 
             // Iterate through all items ("messages") in a folder
             var fi = folder.Items;
@@ -78,44 +78,63 @@ namespace ConsoleApp1
                         DateTime timeToChack = DateTime.Now;
                         
                         if (mi.CreationTime < timeToChack.AddMinutes(-5)) {
-                            Console.Write("old mail" + mi.CreationTime +  "\n");
+
                             continue;
                         }
-                        Console.Write(mi.CreationTime);
                         
-                        if (attachments.Count != 0)
-                        {
-                           
-                            //Console.WriteLine(mi.Sender.Address);
-                            //Console.WriteLine(mi.Subject + " [" + attachments.Count + "]");
-                            //Console.WriteLine(generateFolder(folder.FolderPath, mi.Sender.Address));
-                            for (int i = 1; i <= mi.Attachments.Count; i++)
-                            {
-                                var fn = mi.Attachments[i].FileName.ToLower();
-                                //check wither any of the strings in the extensionsArray are contained within the filename
-                                if (extensionsArray.Any(fn.Contains))
-                                {
+                        
 
-                                    // Create a further sub-folder for the sender
-                                    if (!Directory.Exists(basePath))
+                        if (attachments.Count != 0)
+                            {
+                                //Console.WriteLine(mi.Sender.Address);
+                                //Console.WriteLine(mi.Subject + " [" + attachments.Count + "]");
+                                //Console.WriteLine(generateFolder(folder.FolderPath, mi.Sender.Address));
+                                for (int i = 1; i <= mi.Attachments.Count; i++)
+                                {
+                                    var fn = mi.Attachments[i].FileName.ToLower();
+                                    //check wither any of the strings in the extensionsArray are contained within the filename
+                                    if (extensionsArray.Any(fn.Contains))
                                     {
-                                        Directory.CreateDirectory(basePath);
-                                    }
-                                    totalfilesize = totalfilesize + mi.Attachments[i].Size;
-                                    if (!File.Exists(basePath + mi.Attachments[i].FileName))
-                                    {
-                                        Console.WriteLine("Saving " + mi.Attachments[i].FileName);
-                                        mi.Attachments[i].SaveAsFile(basePath + mi.Attachments[i].FileName);
-                                        //mi.Attachments[i].Delete();
-                                    }
-                                    else
-                                    {
-                                        
-                                        Console.WriteLine("Already saved " + mi.Attachments[i].PropertyAccessor);
+
+                                        // Create a further sub-folder for the sender
+                                        if (!Directory.Exists(basePath))
+                                        {
+
+                                            Directory.CreateDirectory(basePath);
+                                        }
+                                        totalfilesize = totalfilesize + mi.Attachments[i].Size;
+                                        if (!File.Exists(basePath + mi.Attachments[i].FileName))
+                                        {
+                                            Console.WriteLine("Saving " + mi.Attachments[i].FileName);
+                                            mi.Attachments[i].SaveAsFile(basePath + mi.Attachments[i].FileName);
+
+
+
+                                            ProcessStartInfo info = new ProcessStartInfo();
+                                            info.Verb = "print";
+                                            info.FileName = basePath + mi.Attachments[i].FileName;
+                                            info.CreateNoWindow = true;
+                                            info.WindowStyle = ProcessWindowStyle.Hidden;
+
+                                            Process p = new Process();
+                                            p.StartInfo = info;
+                                            p.Start();
+
+                                            p.WaitForInputIdle();
+                                            System.Threading.Thread.Sleep(3000);
+                                            if (false == p.CloseMainWindow())
+                                                p.Kill();
+
+                                            //mi.Attachments[i].Delete();
+                                        }
+                                        else
+                                        {
+
+                                            Console.WriteLine("Already saved " + mi.Attachments[i].PropertyAccessor);
+                                        }
                                     }
                                 }
                             }
-                        }
                     }
                 }
                 catch (System.Exception e)
@@ -158,51 +177,36 @@ namespace ConsoleApp1
         static void EnumerateAccounts()
         {
             Console.Clear();
-            Console.WriteLine("Outlook Attachment Extractor v0.1");
-            Console.WriteLine("---------------------------------");
-            int id;
+            
             Microsoft.Office.Interop.Outlook.Application Application = new Microsoft.Office.Interop.Outlook.Application();
             Microsoft.Office.Interop.Outlook.Accounts accounts = Application.Session.Accounts;
-
-            string response = "";
+            
             while (true == true)
             {
-
-                id = 1;
-                foreach (Microsoft.Office.Interop.Outlook.Account account in accounts)
+                
+                foreach (Account account in accounts)
                 {
-                    Console.WriteLine(id + ":" + EnumerateAccountEmailAddress(account));
-                    id++;
-                }
-                Console.WriteLine("Q: Quit Application");
-
-                response = Console.ReadLine().ToUpper();
-                if (response == "Q")
-                {
-                    Console.WriteLine("Quitting");
-                    return;
-                }
-                if (response != "")
-                {
-                    if (Int32.Parse(response.Trim()) >= 1 && Int32.Parse(response.Trim()) < id)
+                    //moti@u-btech.com
+                    //alondi@sonol.co.il
+                    if (account.DisplayName == "maildalkanim@sonol.co.il")
                     {
-                        Console.WriteLine("Processing: " + accounts[Int32.Parse(response.Trim())].DisplayName);
-                        Console.WriteLine("Processing: " + EnumerateAccountEmailAddress(accounts[Int32.Parse(response.Trim())]));
-
+                        int id = accounts.Count;
                         Microsoft.Office.Interop.Outlook.Folder selectedFolder = Application.Session.DefaultStore.GetRootFolder() as Microsoft.Office.Interop.Outlook.Folder;
-                        selectedFolder = GetFolder(@"\\" + accounts[Int32.Parse(response.Trim())].DisplayName);
+                        selectedFolder = GetFolder(@"\\" + accounts[id].DisplayName);
                         EnumerateFolders(selectedFolder);
-                        Console.WriteLine("Finished Processing " + accounts[Int32.Parse(response.Trim())].DisplayName);
-                        Console.WriteLine("");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("\n" + "- Refrashing in 10 secondes.....");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        System.Threading.Thread.Sleep(300000);
+                        
                     }
-                    else
-                    {
-                        Console.WriteLine("Invalid Account Selected");
-                    }
+                    
+                   }
+                
                 }
             }
 
-        }
+        
 
         // Returns Folder object based on folder path
         static Microsoft.Office.Interop.Outlook.Folder GetFolder(string folderPath)

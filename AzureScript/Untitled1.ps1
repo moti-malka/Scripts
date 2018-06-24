b7d9e4fc718100385284050fa8a76c10dd600a2e
Moti Malka <Moti@u-btech.com>
2018-06-24 11:16:27 +0300

    Signed-off-by: Moti Malka <Moti@u-btech.com>

--- AzureScript/Untitled1.ps1
+++
@@ -1,4 +0,0 @@
-﻿
-$connectionString = "Server=tcp:sqlserver-test-moti.database.windows.net,1433;Initial Catalog=RestoredDatabase;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
-$Ctx = New-AzureSqlDatabaseServerContext -ConnectionString $connectionString
-Get-AzureStorageTable –Context $Ctx | select Name
